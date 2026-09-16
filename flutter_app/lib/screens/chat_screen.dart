cf63f9b20efe65a07e2e6a8ab8986cfdc52eb2d9
lx00924-lx <lx00924@gmail.com>
2026-09-17 00:02:02 +0800

    refactor: clean up imports and dependencies
    Standardize imports across chat screen, settings, and URL helper utilities to improve code consistency.

--- flutter_app/lib/screens/chat_screen.dart
+++ flutter_app/lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import '../models/app_settings.dart';
+import '../models/chat_message.dart';
 import '../models/chat_session.dart';
 import '../providers/chat_provider.dart';
 import '../providers/settings_provider.dart';
--- flutter_app/lib/utils/url_launcher_helper.dart
+++ flutter_app/lib/utils/url_launcher_helper.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class UrlLauncherHelper {
